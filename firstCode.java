46067c5a28ab3f32676e09b2e1732adfb0efe287
Stuti Jain <stuti18022006@gmail.com>
2026-01-21 08:42:31 +0530

    Create firstCode.java with main method
    Initialize a Java program to read command line arguments.

---
+++ firstCode.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class firstCode {
+    public static void main(String[] args){
+        String str[] = new String[5];
+        int i;
+
+        Scanner s = new Scanner(System.in);
+       
+        for (i=0; i<5; i++){
+            System.out.println(args[i]);
+        }
+
+
+    }
+}
